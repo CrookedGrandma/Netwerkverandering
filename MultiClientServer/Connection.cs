1a60ff65b0e8a5d16e524016db9f095a94b90a58
CrookedGrandma <k.t.noot@live.nl>
2017-12-22 23:32:01 +0100

    afvanging dinges

--- MultiClientServer/Connection.cs
+++ MultiClientServer/Connection.cs
@@ -17,17 +17,17 @@ namespace MultiClientServer {
         // Connection heeft 2 constructoren: deze constructor wordt gebruikt als wij CLIENT worden bij een andere SERVER
         public Connection(int port) {
             TcpClient client;
-            /*connect:
+            connect:
             try { client = new TcpClient("localhost", port); }
             catch {
                 Thread.Sleep(10);
                 goto connect;
             }
-            while (!client.Connected) {
+            /*while (!client.Connected) {
                 Thread.Sleep(10);
                 client.Connect("localhost", port);
             }*/
-            client = new TcpClient("localhost", port);
+            //client = new TcpClient("localhost", port);
             Read = new StreamReader(client.GetStream());
             Write = new StreamWriter(client.GetStream());
             Write.AutoFlush = true;
