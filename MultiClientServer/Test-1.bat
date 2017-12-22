6c71e48b80e84992661ba2bfb50a207a8c804428
bennie010697 <bennie.veldhuijzen@gmail.com>
2017-12-22 17:50:43 +0100

    nieuwe bat btw

--- MultiClientServer/Test-1.bat
+++ MultiClientServer/Test-1.bat
@@ -1,5 +1,18 @@
 @echo off
 cd obj\x86\Debug
-start MultiClientServer.exe 1100 1101
-start MultiClientServer.exe 1101 1100 1102
-start MultiClientServer.exe 1102 1101
+start MultiClientServer.exe 55500 55501
+start MultiClientServer.exe 55501 55500 55502
+start MultiClientServer.exe 55502 55501 55503
+start MultiClientServer.exe 55503 55502 55504
+start MultiClientServer.exe 55504 55503 55505
+start MultiClientServer.exe 55505 55504 55506
+start MultiClientServer.exe 55506 55505 55507
+start MultiClientServer.exe 55507 55506 55508
+start MultiClientServer.exe 55508 55507 55509
+start MultiClientServer.exe 55509 55508 55510
+start MultiClientServer.exe 55510 55509 55511
+start MultiClientServer.exe 55511 55510 55512
+start MultiClientServer.exe 55512 55511 55513
+start MultiClientServer.exe 55513 55512 55514
+start MultiClientServer.exe 55514 55513
+
